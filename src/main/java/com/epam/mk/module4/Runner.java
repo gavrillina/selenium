e10304483a6f3c37bece7fe3c49c6eee3899c55b
Kazbek_Muslayev <Kazbek_Muslayev@EPKZKARW0270.moscow.epam.com>
2017-12-13 17:31:39 +0600

    98789

---
+++ src/main/java/com/epam/mk/module4/Runner.java
@@ -0,0 +1,26 @@
+package com.epam.mk.module4;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Runner 
+{
+    public static void main( String[] args )
+    {
+    	WebDriver driver;
+    	 System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+         driver = new ChromeDriver();
+         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+         driver.manage().window().maximize();
+         driver.get("https://protonmail.com");
+         driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-1']/ul/li[7]/a")).click();
+         driver.findElement(By.xpath("//*[@id='username']")).sendKeys("automationTest@protonmail.com");
+         driver.findElement(By.xpath("//*[@id='password']")).sendKeys("test123456");
+         driver.findElement(By.xpath("//*[@id='login_btn']")).click();
+         driver.findElement(By.xpath("//*[@id='pm_sidebar']/button")).click();
+    }
+}
