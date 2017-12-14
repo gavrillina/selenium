2f413785d4f7c0013f9b51e14e4d1b76107dbdd6
Kazbek_Muslayev <Kazbek_Muslayev@EPKZKARW0270.moscow.epam.com>
2017-12-14 18:01:46 +0600

    iuhgi

--- src/main/java/com/epam/mk/module4/Runner.java
+++ src/main/java/com/epam/mk/module4/Runner.java
@@ -1,27 +1,20 @@
 package com.epam.mk.module4;
 
-import java.util.concurrent.TimeUnit;
-
+import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
 public class Runner {
 	public static void main(String[] args) {
-		// createDruft();
-		searchDruftAndSend();
+		createDruft();
+		//searchDruftAndSend();
 	}
 
 	public static void createDruft() {
-		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+		WebDriver driver = new Connector().webdrv();
 		driver.get("https://protonmail.com");
 		driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-1']/ul/li[7]/a")).click();
 		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("hashmap@protonmail.com");
@@ -39,21 +32,24 @@ public class Runner {
 		bodyMessage.perform();
 		driver.switchTo().defaultContent();
 		driver.findElement(By.xpath("//button[@class='composer-btn-save']")).click();
+		driver.close();
 	}
 
 	public static void searchDruftAndSend() {
-		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+		WebDriver driver = new Connector().webdrv();
 		driver.get("https://protonmail.com");
 		driver.findElement(By.xpath("//*[@id='bs-example-navbar-collapse-1']/ul/li[7]/a")).click();
 		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("hashmap@protonmail.com");
 		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456qw");
 		driver.findElement(By.xpath("//button[@id='login_btn']")).click();
 		driver.findElement(By.xpath("//a[@href='/drafts']")).click();
-
+		List<WebElement> druftList = (List<WebElement>) driver
+				.findElements(By.xpath("//*[@ng-repeat = 'conversation in conversations track by conversation.ID']"));
+		for (WebElement wlmt : druftList) {
+			if (wlmt.findElement(By.xpath("//*[@class = 'senders-name']")).getText().equals("test@mail.ru")
+					&& wlmt.findElement(By.xpath("//*[@class = 'subject-text ellipsis']")).getText().equals("My Subject")) {
+				wlmt.click();
+			}
+		}
 	}
 }
