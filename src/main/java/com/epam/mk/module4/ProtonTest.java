5812a696cf68af6222b59f0bfb123aa5d5956cef
Kazbek_Muslayev <Kazbek_Muslayev@EPKZKARW0270.moscow.epam.com>
2017-12-22 10:42:04 +0600

    test fix, properties

--- src/main/java/com/epam/mk/module4/ProtonTest.java
+++ src/main/java/com/epam/mk/module4/ProtonTest.java
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
@@ -15,30 +14,30 @@ public class ProtonTest {
 	CreateDruftPage createDruftPage;
 	SendDruftPage sendDruftPage;
 
-	@BeforeClass(enabled = true)
+	@BeforeClass
 	private void initDriver() {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		ChromeOptions chromeOption = new ChromeOptions();
-		chromeOption.setBinary("C:\\Users\\user\\Desktop\\chrome\\chrome.exe");
-		driver = new ChromeDriver(chromeOption);
+		//ChromeOptions chromeOption = new ChromeOptions();
+		//chromeOption.setBinary("C:\\Users\\user\\Desktop\\chrome\\chrome.exe");
+		driver = new ChromeDriver();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
-	@Test(enabled = true)
+	@Test
 	public void loginPageTest() {
 		Assert.assertNotNull(createDruftPage = new LoginPage(driver).openUrl().loginAction());
 	}
 
-	@Test(dependsOnMethods = { "loginPageTest" }, enabled = true)
+	@Test(dependsOnMethods = { "loginPageTest" })
 	public void createDruftPageTest() {
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		Assert.assertNotNull(sendDruftPage = createDruftPage.createDruft().searchDruft());
 		// Assert.assertNotNull(sendDruftPage = createDruftPage.searchDruft()); // ЧТОБЫ ПРОПУСТИТЬ СОЗДАНИЕ ПИСЬМА И НАЧАТЬ С ПОИСКА В ЧЕРНОВИКАХ
 	}
 
-	@Test(dependsOnMethods = { "createDruftPageTest" }, enabled = true)
+	@Test(dependsOnMethods = { "createDruftPageTest" })
 	public void sendDruftPageTest() throws InterruptedException {
 		Assert.assertEquals(sendDruftPage.sendAction(), "Now your email is in SENT folder");
 	}
