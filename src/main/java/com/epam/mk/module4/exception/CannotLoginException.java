f20168e3d68ee1432178e9721d89cea8a17e476d
user <user@192.168.1.31>
2017-12-24 00:54:00 +0600

    message

--- src/main/java/com/epam/mk/module4/exception/CannotLoginException.java
+++ src/main/java/com/epam/mk/module4/exception/CannotLoginException.java
@@ -3,7 +3,7 @@ package com.epam.mk.module4.exception;
 public class CannotLoginException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	public CannotLoginException(String s) {
-		super(s);
+	public CannotLoginException(String message) {
+		super(message);
 	}
 }
--- src/main/java/com/epam/mk/module4/exception/DraftNotFoundException.java
+++ src/main/java/com/epam/mk/module4/exception/DraftNotFoundException.java
@@ -3,7 +3,7 @@ package com.epam.mk.module4.exception;
 public class DraftNotFoundException extends Exception {
 	private static final long serialVersionUID = 1L;
 
-	public DraftNotFoundException(String s) {
-		super(s);
+	public DraftNotFoundException(String message) {
+		super(message);
 	}
 }
