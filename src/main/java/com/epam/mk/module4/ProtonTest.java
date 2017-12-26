0b9242827a638e0826d1800f8bfce8e7367c9f85
Kazbek_Muslayev <Kazbek_Muslayev@EPKZKARW0270.moscow.epam.com>
2017-12-26 15:14:28 +0600

    comments removed

--- src/main/java/com/epam/mk/module4/ProtonTest.java
+++ src/main/java/com/epam/mk/module4/ProtonTest.java
@@ -36,6 +36,21 @@ public class ProtonTest {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();	
 
+		// System.setProperty("webdriver.gecko.driver",
+		// "src/main/resources/geckodriver_x64.exe");
+		// FirefoxProfile firefoxProfile = new FirefoxProfile(new
+		// File("C:\\Users\\Kazbek_Muslayev\\Desktop\\firefox_x64_portable\\profile_selenium"));
+		// FirefoxOptions firefoxOptions = new FirefoxOptions();
+		// firefoxOptions.setBinary("C:\\Users\\Kazbek_Muslayev\\Desktop\\firefox_x64_portable\\application\\firefox.exe");
+		// firefoxOptions.setProfile(firefoxProfile);
+		// driver = new FirefoxDriver(firefoxOptions);
+		// driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// driver.manage().window().maximize();
+		
+		
+		
+		
 		// System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 //		// ChromeOptions chromeOption = new ChromeOptions();
 //		// chromeOption.setBinary("C:\\Users\\user\\Desktop\\chrome\\chrome.exe");
--- src/main/java/com/epam/mk/module4/page/move/JqueryuiTest.java
+++ src/main/java/com/epam/mk/module4/page/move/JqueryuiTest.java
@@ -15,17 +15,6 @@ public class JqueryuiTest {
 
 	@BeforeTest
 	private void initDriver() {
-		// System.setProperty("webdriver.gecko.driver",
-		// "src/main/resources/geckodriver_x64.exe");
-		// FirefoxProfile firefoxProfile = new FirefoxProfile(new
-		// File("C:\\Users\\Kazbek_Muslayev\\Desktop\\firefox_x64_portable\\profile_selenium"));
-		// FirefoxOptions firefoxOptions = new FirefoxOptions();
-		// firefoxOptions.setBinary("C:\\Users\\Kazbek_Muslayev\\Desktop\\firefox_x64_portable\\application\\firefox.exe");
-		// firefoxOptions.setProfile(firefoxProfile);
-		// driver = new FirefoxDriver(firefoxOptions);
-		// driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		// driver.manage().window().maximize();
 
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/webdrivers/chromedriver_x86.exe");
 		ChromeOptions chromeOption = new ChromeOptions();
