446203fc040cf589573fa3b2f622bade92b7a79f
Kazbek_Muslayev <Kazbek_Muslayev@EPKZKARW0270.moscow.epam.com>
2017-12-26 13:31:18 +0600

    grid

---
+++ src/main/resources/02_add_chromedriver.bat
@@ -0,0 +1,6 @@
+set SERVER_VERSION=3.8.1
+set HUB_PORT=4444
+set NODE_PORT=5557
+set REGISTER_IP=localhost
+set CHROME_DRIVER=drivers\chromedriver_x86.exe
+java -Dwebdriver.chrome.driver="%CHROME_DRIVER%" -jar selenium-server-standalone-%SERVER_VERSION%.jar -role node -hub http://%REGISTER_IP%:%HUB_PORT%/grid/register -browser "browserName=chrome,platform=WINDOWS" -port %NODE_PORT%
